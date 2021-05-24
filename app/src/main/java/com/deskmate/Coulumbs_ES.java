51c7bd8a5c72ce74468e7ea5de4d878a15a59934
Nandini Garg <55273422+hellofrom-nandinigarg@users.noreply.github.com>
2021-05-24 09:07:31 +0530

    Variable calc, non-functional code

--- app/src/main/java/com/deskmate/Coulumbs_ES.java
+++ app/src/main/java/com/deskmate/Coulumbs_ES.java
@@ -7,12 +7,15 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.deskmate.DeskmateRes.*;
+
 public class Coulumbs_ES extends AppCompatActivity {
     private EditText entry1;
     private EditText entry2;
     private EditText entry3;
+    private EditText entry4;
     private TextView result;
-    static final double k = 9*Math.pow(10,9);
+
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
@@ -20,19 +23,31 @@ public class Coulumbs_ES extends AppCompatActivity {
         entry1 = (EditText) findViewById(R.id.charge1);
         entry2 = (EditText) findViewById(R.id.charge2);
         entry3 = (EditText) findViewById(R.id.dist);
+        //entry4 = (EditText) findViewById(R.id.force_cl);
         result = (TextView) findViewById(R.id.result_ef);
     }
-    private double calcFunc(double q1, double q2, double f){
+    private void calcFunc(double q1, double q2, double r){
+
+//        if (entry3.getText().toString().isEmpty()) {
+//            result.setText(String.valueOf(com.deskmate.DeskmateRes.expConverter(Double.toString(Math.sqrt(DeskmateRes.coulumbsCnst * q1 * q2 / f)))+"m"));
+//        } else if (entry1.getText().toString().isEmpty()){
+//            result.setText(String.valueOf(com.deskmate.DeskmateRes.expConverter(Double.toString(f* (r * r)/(DeskmateRes.coulumbsCnst *q1)))+"C"));
+//        } else if (entry2.getText().toString().isEmpty()){
+//            result.setText(String.valueOf(com.deskmate.DeskmateRes.expConverter(Double.toString(f* (r * r)/(DeskmateRes.coulumbsCnst *q2)))+"C"));
+//        } else if (entry4.getText().toString().isEmpty()){
+//            result.setText(String.valueOf(com.deskmate.DeskmateRes.expConverter(Double.toString(Math.sqrt(DeskmateRes.coulumbsCnst * q1 * q2 / (r*r))))+"N"));
+//        }
+        result.setText(String.valueOf(expConverter(Double.toString(9*Math.pow(10,9) * q1 * q2 / (r*r)))+" N"));
+    }
+
+    public void Coulumbs_es(View view){
+// add variable calculators
 
         double q1 = Double.parseDouble(entry1.getText().toString());
         double q2 = Double.parseDouble(entry2.getText().toString());
-
-    }
-    public void Coulumbs_es(View view){
-
-
         double r = Double.parseDouble(entry3.getText().toString());
-        result.setText(String.valueOf(com.deskmate.DeskmateRes.expConverter(Double.toString(k * q1 * q2 / (r * r)))));
+        //double f = Double.parseDouble(entry4.getText().toString());
+        calcFunc(q1, q2, r);
 
 
     }
