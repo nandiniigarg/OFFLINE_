207817083502780fb91839c3a67521886213690e
Chaiitanyaa <Chchopra12@gmail.com>
2021-05-29 16:13:46 +0530

    updates from Chaiitanyaa

--- app/src/main/java/com/deskmate/Current_Electricity.java
+++ app/src/main/java/com/deskmate/Current_Electricity.java
@@ -1,6 +1,5 @@
 package com.deskmate;
 
-import static com.deskmate.DeskmateRes.coulumbsCnst;
 import static com.deskmate.DeskmateRes.df;
 import static com.deskmate.DeskmateRes.expConverter;
 
@@ -38,4 +37,52 @@ public class Current_Electricity {
             return "ERROR";
         }
     }
+    
+    
+    public static String Wbridge(String p, String q, String r, String s)
+    {
+
+        double R1 = Double.parseDouble(p);
+        double R2 = Double.parseDouble(q);
+        double R3 = Double.parseDouble(r);
+        double R4 = Double.parseDouble(s);
+
+        if (p.equals("*")) {
+            String res = String.valueOf(Double.parseDouble(df.format(((R2 * R3) / R4) +" Ohm ")));
+            if (res.contains("E")) {
+                return (expConverter(res));
+            } else {
+                return (res);
+            }
+
+        } else if (q.equals("*")) {
+            String res = String.valueOf(Double.parseDouble(df.format(((R4 * R1)/ R3) +" Ohm ")));
+            if (res.contains("E")) {
+                return (expConverter(res));
+            } else {
+                return (res);
+            }
+        } else if (r.equals("*")) {
+            String res = String.valueOf(Double.parseDouble(df.format(((R1*R4) / R2) +" Ohm ")));
+            if (res.contains("E")) {
+                return (expConverter(res));
+            } else {
+                return (res);
+            }
+        } else if (s.equals("*")) {
+            String res = String.valueOf(Double.parseDouble(df.format(((R2*R3) / R1 ) +" Ohm ")));
+            if (res.contains("E")) {
+                return (expConverter(res));
+            } else {
+                return (res);
+            }
+        }
+        else {
+        return "ERROR";
+        }
+    }
 }
+
+
+
+
